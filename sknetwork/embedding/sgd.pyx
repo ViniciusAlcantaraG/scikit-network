e5628a5aaa1a7e946a43aaae83ec590030ff8611
ViniciusA <vinir570@gmail.com>
2026-09-21 15:05:45 +0200

    Gamma in stochastic gradient descent

--- sknetwork/embedding/sgd.pyx
+++ sknetwork/embedding/sgd.pyx
@@ -9,11 +9,11 @@ import numpy as np
 cpdef sgd(int n_components, int n_epochs, int n, int[:] row, int[:] col,
             double[:,:] low_dim, float a, float b, float lr,
             int negative_sampling_rate, double[:] epochs_per_sample, double[:] epoch_of_next_sample,
-            unsigned int seed=0):
+            unsigned int seed=0, float gamma=1.0):
 
     srand(seed)
     cdef int num_samples = epochs_per_sample.shape[0]
-    cdef int epoch, idx, i, j, k, n_neg_samples
+    cdef int epoch, idx, i, j, k
     cdef double alpha, temp, coef, grad, d
     cdef double[::1] delta = np.empty(n_components, dtype=np.float64)
 
@@ -33,8 +33,8 @@ cpdef sgd(int n_components, int n_epochs, int n, int[:] row, int[:] col,
 
                 for k in range(n_components):
                     grad = coef * delta[k]
-                    low_dim[i,k] -= lr * alpha * grad
-                    low_dim[j,k] += lr * alpha * grad
+                    low_dim[i,k] += lr * alpha * grad
+                    low_dim[j,k] -= lr * alpha * grad
 
                 for _ in range(negative_sampling_rate):
                     j = rand() % n
@@ -44,11 +44,11 @@ cpdef sgd(int n_components, int n_epochs, int n, int[:] row, int[:] col,
                         d += delta[k] * delta[k]
 
                     temp = pow(d + 1e-6, b - 1)
-                    coef = 2*a*b*temp/ (1.0 + a * d * temp)
+                    coef = 2.0*gamma*b / ((0.001 + d) * (1.0 + a * d * temp))
 
                     for k in range(n_components):
                         grad = coef * delta[k]
-                        low_dim[i,k] -= lr * alpha * grad
+                        low_dim[i,k] += lr * alpha * grad
 
                 epoch_of_next_sample[idx] += epochs_per_sample[idx]
 
